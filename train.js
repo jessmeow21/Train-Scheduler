5f52e5191c767cbd59aec08e73f634867e626441
Jessica <jessica.sulaiman86@gmail.com>
2019-07-03 15:47:44 -0400

    firebase return

---
+++ train.js
@@ -0,0 +1,16 @@
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBF1W_xcyYpiSc2-aLZIPf-p5FlzC9WBUs",
+        authDomain: "train-project-d1632.firebaseapp.com",
+        databaseURL: "https://train-project-d1632.firebaseio.com",
+        projectId: "train-project-d1632",
+        storageBucket: "",
+        messagingSenderId: "631122644100",
+        appId: "1:631122644100:web:2c35f30fd5b340c9"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+
+    console.log(firebaseConfig);
